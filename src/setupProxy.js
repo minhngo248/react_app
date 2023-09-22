bc7963167c716b7c754dca810d02ec0b0b7a9b3c
Minh Ngo <bbbbbb267@gmail.com>
2023-09-23 02:12:50 +0700

    deploy to Azure

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://expressminhapp.azurewebsites.net',
+            target: 'http://anime-back.francecentral.azurecontainer.io',
             changeOrigin: true,
         })
     );
